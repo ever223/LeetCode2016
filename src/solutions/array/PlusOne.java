62ea758a5d2cee9546a0651a7d6df92339f468fc
xiaoo_gan <85265900@qq.com>
2016-05-03 09:52:24 +0800

    64. Minimum Path Sum 66. Plus One

---
+++ src/solutions/array/PlusOne.java
@@ -0,0 +1,38 @@
+package solutions.array;
+
+import java.util.Arrays;
+
+/**
+ * @AUTHOR: xiaoo_gan
+ * @DATE: 2016-05-03 09:35.
+ * @DESCRIPTION:
+ */
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        if (digits == null || digits.length == 0) {
+            return digits;
+        }
+        int n = digits.length;
+        if (digits[n - 1] < 9) {
+            digits[n - 1] += 1;
+            return digits;
+        }
+        int i = n - 1;
+
+        while (i >= 0 && digits[i] == 9) {
+            digits[i] = 0;
+            i --;
+        }
+        if (i >= 0) {
+            digits[i] += 1;
+            return digits;
+        } else {
+            int[] result = new int[n + 1];
+            result[0] = 1;
+            for (int j = 1; j < result.length; j++) {
+                result[j] = 0;
+            }
+            return result;
+        }
+    }
+}
--- src/solutions/dp/MinimumPathSum.java
+++ src/solutions/dp/MinimumPathSum.java
@@ -17,11 +17,9 @@ public class MinimumPathSum {
         }
         for(int i = 0; i < m; i ++) {
             for(int j = 0; j < n; j ++) {
-                if(i == 0 && j == 0) {
-                    continue;
-                } else if(i == 0) {
+                if(i == 0 && j != 0) {
                     grid[0][j] += grid[0][j - 1];
-                } else if(j == 0) {
+                } else if(j == 0 && i != 0) {
                     grid[i][0] += grid[i -1][0];
                 } else {
                     grid[i][j] += Math.min(grid[i -1][j], grid[i][j - 1]);
