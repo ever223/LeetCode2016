dd3db1d29ed063b8ca439f2d8f7734d57403fe97
xiaoo_gan <85265900@qq.com>
2016-05-04 18:35:21 +0800

    147. Insertion Sort List

---
+++ src/solutions/sort/InsertionSortList.java
@@ -0,0 +1,25 @@
+package solutions.sort;
+
+import solutions.linklist.ListNode;
+
+/**
+ * @AUTHOR: xiaoo_gan
+ * @DATE: 2016-05-04 18:32.
+ * @DESCRIPTION:
+ */
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        while(head != null) {
+            ListNode cursor = dummy;
+            while(cursor.next != null && cursor.next.val < head.val) {
+                cursor = cursor.next;
+            }
+            ListNode temp = head.next;
+            head.next = cursor.next;
+            cursor.next = head;
+            head = temp;
+        }
+        return dummy.next;
+    }
+}
