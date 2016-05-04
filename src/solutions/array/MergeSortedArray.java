21879e351ea125326ac3044e70ef41020b9ee1d5
xiaoo_gan <85265900@qq.com>
2016-05-04 18:53:09 +0800

    88. Merge Sorted Array

---
+++ src/solutions/array/MergeSortedArray.java
@@ -0,0 +1,20 @@
+package solutions.array;
+
+/**
+ * @AUTHOR: xiaoo_gan
+ * @DATE: 2016-05-04 18:52.
+ * @DESCRIPTION:
+ */
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = m + n - 1; i >= 0; i --) {
+            int num1 = (m - 1 < 0) ? Integer.MIN_VALUE : nums1[m - 1];
+            int num2 = (n - 1 < 0) ? Integer.MIN_VALUE : nums2[n - 1];
+            if(num1 <= num2) {
+                nums1[i] = nums2[--n];
+            } else {
+                nums1[i] = nums1[--m];
+            }
+        }
+    }
+}
