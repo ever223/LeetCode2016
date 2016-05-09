bcdea102544f05ad56f4e44c21ea977374256cfc
xiaoo_gan <85265900@qq.com>
2016-05-09 19:15:31 +0800

    258. Add Digits

---
+++ src/solutions/math/AddDigits.java
@@ -0,0 +1,20 @@
+package solutions.math;
+
+/**
+ * @AUTHOR: xiaoo_gan
+ * @DATE: 2016-05-09 19:15.
+ * @DESCRIPTION:
+ */
+public class AddDigits {
+    public int addDigits(int num) {
+        while(num / 10 != 0) {
+            int temp = 0;
+            while(num != 0) {
+                temp += (num % 10);
+                num /= 10;
+            }
+            num = temp;
+        }
+        return num;
+    }
+}
