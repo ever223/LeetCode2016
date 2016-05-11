1b0518da873d2ee9fdf6a033c83f21b3d6097dd6
xiaoo_gan <85265900@qq.com>
2016-05-11 19:48:36 +0800

    283. Move Zeroes

--- src/solutions/array/MoveZeroes.java
+++ src/solutions/array/MoveZeroes.java
@@ -17,4 +17,17 @@ public class MoveZeroes {
             nums[i] = 0;
         }
     }
+
+    public void moveZeroes1(int[] nums) {
+        int zeros = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeros ++;
+            }
+            if (zeros != 0 && nums[i] != 0) {
+                nums[i - zeros] = nums[i];
+                nums[i] = 0;
+            }
+        }
+    }
 }
