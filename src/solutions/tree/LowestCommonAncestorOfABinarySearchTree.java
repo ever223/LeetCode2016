03c24eaf0900cc05f0132f7bc79104ef745f7714
xiaoo_gan <85265900@qq.com>
2016-05-13 17:04:05 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ src/solutions/tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,24 @@
+package solutions.tree;
+
+/**
+ * @AUTHOR: xiaoo_gan
+ * @DATE: 2016-05-12 21:50.
+ * @DESCRIPTION:
+ */
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return null;
+        }
+        if (root == null) {
+            return null;
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
