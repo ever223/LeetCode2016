556dbe0ec29ce713150db83cfb6fe82000645134
xiaoo_gan <85265900@qq.com>
2016-05-29 22:23:23 +0800

    278. First Bad Version

---
+++ src/solutions/binarysearch/FirstBadVersion.java
@@ -0,0 +1,42 @@
+package solutions.binarysearch;
+
+import java.util.Stack;
+
+/**
+ * @AUTHOR: xiaoo_gan
+ * @DATE: 2016-05-28 23:07.
+ * @DESCRIPTION:
+ */
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+public class FirstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) >> 1;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public int firstBadVersion1(int n) {
+        return bs(1, n);
+    }
+    public int bs(int left, int right) {
+        if (left == right) {
+            return left;
+        }
+        int mid = left + ((right - left) >> 1);
+        if (isBadVersion(mid)) {
+            return bs(left, mid);
+        } else {
+           return bs(mid + 1, right);
+        }
+    }
+}
---
+++ src/solutions/binarysearch/VersionControl.java
@@ -0,0 +1,13 @@
+package solutions.binarysearch;
+
+/**
+ * @AUTHOR: xiaoo_gan
+ * @DATE: 2016-05-28 23:08.
+ * @DESCRIPTION:
+ */
+public class VersionControl {
+    boolean isBadVersion(int version) {
+        // TODO
+        return true;
+    }
+}
